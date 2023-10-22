ed365dc3cdbe42872b0a1cb99d8764f01b3aa500
Ryo Nakamine <rnakamine8080@gmail.com>
2023-10-23 06:28:02 +0900

    update call awssig4

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,10 +1,5 @@
-require 'faraday_middleware/aws_sigv4'
-
-Searchkick.client.transport.connections.each do |connection|
-  connection.connection.builder.use(FaradayMiddleware::AwsSigV4, {
-    service: 'es',
-    region: 'ap-northeast-1',
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-  })
-end
+Searchkick.aws_credentials = {
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: 'ap-northeast-1'
+}
